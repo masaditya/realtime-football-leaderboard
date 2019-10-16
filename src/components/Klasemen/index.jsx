d783a2b2353ddae9099d8539b1dfe11fff56df4a
adit <aeka489@gmail.com>
2019-10-16 23:25:57 +0700

    initial socket

--- src/components/Klasemen/index.jsx
+++ src/components/Klasemen/index.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import KlasemenTable from "./KlasemenTable";
+import socketIOClient from 'socket.io-client';
+import axios from 'axios';
 
 export default class Klasemen extends Component {
   constructor() {
     super();
     this.state = {};
   }
+
+  socket = socketIOClient("http://localhost:4000");
+
+  componentWillMount() {
+    // axios.get(this.state.url,).then(response => {
+    //   console.log(response);
+    // })
+
+    // this.socket.on("update", update => {
+    //   console.log(update);
+    // });
+  }
+  
+
   render() {
     return (
       <div>
